cdfe4b4c660b8ddb5ecb795da1ec5c6fbf52fb0f
Vikas-ELF <vikas.k@testyantra.com>
2020-04-09 11:10:12 +0530

    Alumni app inetial code

---
+++ src/main/java/com/alumini/aluminiapp/AluminiAppApplication.java
@@ -0,0 +1,13 @@
+package com.alumini.aluminiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluminiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluminiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alumini/aluminiapp/AluminiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumini.aluminiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluminiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
